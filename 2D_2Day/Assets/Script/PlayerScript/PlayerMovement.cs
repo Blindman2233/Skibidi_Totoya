2b1f7b60c615b0457911e4f8e89effa0bcc55b85
Blindman2233 <99383637+Blindman2233@users.noreply.github.com>
2026-01-25 12:39:58 +0700

    1/25/26 Player animation

--- 2D_2Day/Assets/Script/PlayerScript/PlayerMovement.cs
+++ 2D_2Day/Assets/Script/PlayerScript/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private float moveInput;
+    private Animator animator;
 
     // This variable holds the object we are currently touching
     private GameObject currentInteractable = null;
@@ -14,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -21,11 +23,25 @@ public class PlayerMovement : MonoBehaviour
         // --- 1. Movement Input ---
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        // --- 2. Interaction Input ---
-        // If we press E AND we are currently touching an interactable object
+        // --- 2. Animation Logic ---
+        // Send positive speed to the Animator (0 = Idle, >0 = Run)
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+
+        // --- 3. Flip the Character (Safely) ---
+        // We use Mathf.Abs to keep your current size (0.64) and just change the sign (+/-)
+
+        if (moveInput > 0) // Moving Right
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (moveInput < 0) // Moving Left
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        // --- 4. Interaction Input ---
         if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
         {
-            // Call the function on the object script
             InteractableItem itemScript = currentInteractable.GetComponent<InteractableItem>();
             if (itemScript != null)
             {
@@ -36,31 +52,24 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Move the player (Left/Right)
+        // Move the player
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
     }
 
     // --- TRIGGER DETECTION ---
-
-    // When Player enters the object's area
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Interactable"))
         {
             currentInteractable = other.gameObject;
-            Debug.Log("Can Interact! Press E"); // Helpful message in console
         }
     }
 
-    // When Player leaves the object's area
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Interactable"))
+        if (other.CompareTag("Interactable") && other.gameObject == currentInteractable)
         {
-            if (other.gameObject == currentInteractable)
-            {
-                currentInteractable = null;
-            }
+            currentInteractable = null;
         }
     }
 }
